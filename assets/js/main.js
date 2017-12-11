b6322316f16688577b375ed899d266686c839fa7
LeidyRosmery <rosmery.maldonado89@gmail.com>
2017-12-11 13:27:05 -0500

    repositorio

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+$(".img-servicio").on("click", function() {
+  console.log("hol<s");
+  $(this).find("img").attr('src','assets/img/icono-exterior.png');
+  $(this).find("img").css('height','98px');
+  $(".lista-menu").css("display","block");
+});
