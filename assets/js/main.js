f211c3746c93fbf8e9c546c6713cc27050791ed7
LeidyRosmery <rosm3rita@hotmail.com>
2017-12-14 00:50:20 -0500

    quita todos pero no en si mismo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,30 @@
+const activator = (valor) => {
+    valor.addClass('active');
+    valor.find("img").css('display', 'block');
+    valor.find('span').css('display', 'none');
+    valor.css('background', '#0079d7');
+}
+
+const desactivator = (valor) => {
+    valor.removeClass('active');
+    valor.find("img").css('display', 'none');
+    valor.find('span').css('display', 'block');
+    valor.css('background', '');
+}
+
 $(".img-servicio").on("click", function() {
-  console.log("hol<s");
-  $(this).find("img").attr('src', 'assets/img/icono-exterior.png');
-  $(this).find("img").css('height', '98px');
-  $(".lista-menu").css("display", "block");
+  let valor = false;
+    if (valor == false) {
+        desactivator($(".img-servicio"));
+        activator($(this));
+        valor=true;
+    } else {
+        desactivator($(this));
+        valor=false;
+    }
 });
+
+
+
+
+var piso = ['Instalacion de alfombra o tapizon', 'Instalacion de ceramico y/o porcelanato', 'Instalacion de piso laminado', 'Instalacion de piso Vinilico o LG'];
